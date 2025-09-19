249fa1221ab2001b7211536d2c404f769d1ccf5f
Jibril <jibrilf@lmtlss.dev>
2025-09-19 11:30:51 -0500

    Added main header file. Added sphere-ray intersect detection.

---
+++ src/include/main.h
@@ -0,0 +1,9 @@
+#include "color.h"
+#include "ray.h"
+#include "3dvec.h"
+
+// Detect sphere intersections.
+bool sphere_intersect(const point3d&, double radius, const ray& r);
+
+// Determine ray color.
+color ray_color(const ray&);
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
 #include "include/color.h"
 #include "include/ray.h"
 #include "include/3dvec.h"
+#include "include/main.h"
 
 #include <iostream>
 
+
 color ray_color(const ray& r) {
+	if (sphere_intersect(point3d(0, 0, -1), 0.5, r)) return color(1, .75, .8);
+
+
 	vec3d unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0-a) * color(1.0,1.0,1.0) + a * color(0.5, 0.9, 1.0);
+}
+
+bool sphere_intersect(const point3d& center, double radius, const ray& r) {
+	vec3d oc = center - r.origin();
+	auto a = dot(r.direction(), r.direction());
+	auto b = -2.0 * dot(r.direction(), oc);
+	auto c = dot(oc, oc) - radius*radius;
+	auto discriminant = b*b - 4*a*c;
+	return (discriminant >= 0);
 }
 
 int main() {
